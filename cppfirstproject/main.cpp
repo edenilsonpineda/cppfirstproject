c7ee5968ec2e68e32ab9009a3e357327dffd7f77
Edenilson Pineda <edenilsonpineda@outlook.com>
2020-04-11 20:26:07 -0600

    Initial Commit

---
+++ cppfirstproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppfirstproject
+//
+//  Created by Edenilson Pineda on 4/11/20.
+//  Copyright © 2020 Edenilson Pineda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
